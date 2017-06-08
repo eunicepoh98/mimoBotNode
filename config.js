0457f3a18f8b5b4b8f3a61186d5fb45baad992b5
Eunice Poh <eunicepoh98@yahoo.com.sg>
2017-06-08 18:57:04 +0800

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-var config = module.exports = {}
-
-config.dbCredentials = {
-    'database': 'mimobotdb',
-    'username': 'root',
-    'password': '12345',
-    "host": "localhost",
-    "dialect": "mysql"
-}
