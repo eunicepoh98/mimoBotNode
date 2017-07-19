a9d200dbbd83ff028868d4cb609ae9cd19b7fa85
inbfi1niate4 <eunicepoh98@yahoo.com.sg>
2017-07-20 00:41:38 +0800

    load synonyms as values

--- APIs/witdata.js
+++ APIs/witdata.js
@@ -7,23 +7,23 @@ var jobfunction = require(path.resolve('./APIs/jobfunction.js'));
 var industry = require(path.resolve('./APIs/industry.js'));
 var jobtype = require(path.resolve('./APIs/jobtype.js'));
 
-function loadJobType() {
+witdata.loadJobFunction = function loadJobFunction() {
     return new Promise(function (resolve, reject) {
-        jobfunction.getAllJobFunctionName().then(function (data) {
-            resolve(createEntity("job_type", "contains all the job type", formatData(data)));
-        });
-    });
-}
-function loadJobFunction() {
-    return new Promise(function (resolve, reject) {
-        jobtype.getAllJobTypeName().then(function (data) {
+        jobfunction.getAllJobFunctionSynonyms().then(function (data) {
             resolve(createEntity("job_function", "contains all the job function", formatData(data)));
         });
     });
 }
-function loadIndustry() {
+witdata.loadJobType = function loadJobType() {
     return new Promise(function (resolve, reject) {
-        industry.getAllIndustryName().then(function (data) {
+        jobtype.getAllJobTypeSynonyms().then(function (data) {
+            resolve(createEntity("job_type", "contains all the job type", formatData(data)));
+        });
+    });
+}
+witdata.loadIndustry = function loadIndustry() {
+    return new Promise(function (resolve, reject) {
+        industry.getAllIndustrySynonyms().then(function (data) {
             resolve(createEntity("industry_type", "contains all the industry", formatData(data)));
         });
     });
@@ -35,20 +35,27 @@ function Value(value, expressions) {
 function formatData(data) {
     var array = [];
     data.forEach(function (value) {
-        if (value.includes("/")) {
-            value.split(" / ").forEach(function (one) {
-                array.push({ value: one })
-            })
-        } else if (value.includes("&")) {
-            value.split(" & ").forEach(function (one) {
-                array.push({ value: one })
-            })
-        } else {
-            array.push({ value: value })
-        }
+        array.push({ value: value });
     });
     return array;
 }
+// function formatData(data) {
+//     var array = [];
+//     data.forEach(function (value) {
+//         if (value.includes("/")) {
+//             value.split(" / ").forEach(function (one) {
+//                 array.push({ value: one })
+//             })
+//         } else if (value.includes("&")) {
+//             value.split(" & ").forEach(function (one) {
+//                 array.push({ value: one })
+//             })
+//         } else {
+//             array.push({ value: value })
+//         }
+//     });
+//     return array;
+// }
 function createEntity(entityName, description, value) {
     var entities = {
         "doc": description,
@@ -74,52 +81,7 @@ function createEntity(entityName, description, value) {
 
 witdata.loadAllData = function () {
     return new Promise(function (resolve, reject) {
-        loadIndustry().then(loadJobFunction().then(loadJobType().then(resolve)));
+        witdata.loadIndustry().then(witdata.loadJobFunction().then(witdata.loadJobType().then(resolve)));
     });
 }
-
-var jobTypeData = ["Full Time", "Part Time", "Contract"];
-
-var industryData = [
-    'Aerospace / Aviation',
-    'Agriculture / Forestry / Fishing',
-    'Athletics / Sports',
-    'Arts & Culture',
-    'Automotive',
-    'Banking / Financial Services / Insurance',
-    'Broadcast / Media / Entertainment',
-    'Charities / Social Services / NGOs',
-    'Clothing / Garment / Textile',
-    'Conglomerate / Diversified Companies',
-    'Construction / Building Services / Real Estate',
-    'Education / Training Institutes',
-    'Energy / Power / Environmental',
-    'Executive Search & Recruitment Agencies',
-    'Fast Moving Consumer Goods',
-    'Human Resources Management / Consultancy'
-];
-
-var jobFunctionData = [
-    'Administrator / Office Manager',
-    'Architect / Interior Design / Builder',
-    'Banking, Financial Services & Insurance',
-    'Customer Service & Contact Centre',
-    'Designer',
-    'Educator / Trainer',
-    'Finance & Accounting',
-    'Healthcare & Wellness',
-    'Human Resources',
-    'IT - Hardware',
-    'IT - Software',
-    'Legal & Compliance',
-    'Manufacturing / Production / Engineering',
-    'Marketing, Public Relations & Communications',
-    'Media & Entertainment',
-    'Professionals',
-    'Research & Development',
-    'Sales & Business Development',
-    'Supply Chain, Procurement & Logistics',
-    'Telecom / Service Provider',
-    'Headhunter & Recruiter'
-];
 
