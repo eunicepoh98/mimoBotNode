ff101859fa35dac6da5a50fcdc3f13629e62868e
inbfi1niate4 <eunicepoh98@yahoo.com.sg>
2017-07-19 17:02:17 +0800

    auto create records in industry, job function, job type, country and currency

--- Models/index.js
+++ Models/index.js
@@ -37,22 +37,96 @@ Object.keys(db).forEach(function (modelName) {
   }
 });
 
+var reset = false;
 sequelize.sync({
-  force: true // force: true will drop the table if it already exists
+  force: reset // force: true will drop the table if it already exists
   //logging:console.log
+}).then(() => {
+  if (reset) {
+    db['Industry'].bulkCreate([
+      { IndustryID: 1, IndustryName: 'Aerospace / Aviation' },
+      { IndustryID: 2, IndustryName: 'Agriculture / Forestry / Fishing' },
+      { IndustryID: 3, IndustryName: 'Automotive' },
+      { IndustryID: 4, IndustryName: 'Broadcast / Media / Entertainment' },
+      { IndustryID: 5, IndustryName: 'Charities / Social Services / NGOs' },
+      { IndustryID: 6, IndustryName: 'Construction / Building Services / Real Estate' },
+      { IndustryID: 7, IndustryName: 'Education / Training Institutes' },
+      { IndustryID: 8, IndustryName: 'Energy / Power / Environmental' },
+      { IndustryID: 9, IndustryName: 'Fast Moving Consumer Goods' },
+      { IndustryID: 10, IndustryName: 'Human Resources Management / Consultancy' },
+      { IndustryID: 11, IndustryName: 'Government / Civil Service / GLC (Govt-Linked Co.)' },
+      { IndustryID: 12, IndustryName: 'Hospital / Healthcare' },
+      { IndustryID: 13, IndustryName: 'Hospitality / Food & Beverages / Retail' },
+      { IndustryID: 14, IndustryName: 'Industrial / Engineering' },
+      { IndustryID: 15, IndustryName: 'Information Technology / Telecommunications' },
+      { IndustryID: 16, IndustryName: 'Manufacturing' },
+      { IndustryID: 17, IndustryName: 'Oil & Gas / Chemical / Mining' },
+      { IndustryID: 18, IndustryName: 'Pharmaceutical / Medical' },
+      { IndustryID: 19, IndustryName: 'Professional Services (e.g. Accounts & Audit, Legal, Consulting' },
+      { IndustryID: 20, IndustryName: 'Shipbuilding / Marine Services' },
+      { IndustryID: 21, IndustryName: 'Transportation / Logistics' },
+      { IndustryID: 22, IndustryName: 'Others' },
+      { IndustryID: 23, IndustryName: 'Banking / Financial Services / Insurance' },
+      { IndustryID: 24, IndustryName: 'Executive Search & Recruitment Agencies' }
+    ]).then(() => {
+      console.log("Industries Added");
+      db['JobFunction'].bulkCreate([
+        { JobFunctionID: 1, JobFunctionName: 'Top Management' }, //(e.g. CEO / CFO / COO, etc.)
+        { JobFunctionID: 2, JobFunctionName: 'Senior Management' }, //(e.g. VP / Director, etc.)
+        { JobFunctionID: 3, JobFunctionName: 'Administrator / Office Manager' },
+        { JobFunctionID: 4, JobFunctionName: 'Architect / Interior Design / Builder' },
+        { JobFunctionID: 5, JobFunctionName: 'Banking, Financial Services & Insurance' },
+        { JobFunctionID: 6, JobFunctionName: 'Customer Service & Contact Centre' },
+        { JobFunctionID: 7, JobFunctionName: 'Educator / Trainer' },
+        { JobFunctionID: 8, JobFunctionName: 'Finance & Accounting' },
+        { JobFunctionID: 9, JobFunctionName: 'Healthcare & Wellness' },
+        { JobFunctionID: 10, JobFunctionName: 'Human Resources' },
+        { JobFunctionID: 11, JobFunctionName: 'Information Technologies' },
+        { JobFunctionID: 12, JobFunctionName: 'Legal & Compliance' },
+        { JobFunctionID: 13, JobFunctionName: 'Manufacturing / Production / Engineering' },
+        { JobFunctionID: 14, JobFunctionName: 'Marketing, Public Relations & Communications' },
+        { JobFunctionID: 15, JobFunctionName: 'Media & Entertainment' },
+        { JobFunctionID: 16, JobFunctionName: 'Professionals' }, //(e.g. Pilot / Diver / Military, etc.)
+        { JobFunctionID: 17, JobFunctionName: 'Research & Development' },
+        { JobFunctionID: 18, JobFunctionName: 'Sales & Business Development' },
+        { JobFunctionID: 19, JobFunctionName: 'Supply Chain, Procurement, Transportation & Logistics' },
+        { JobFunctionID: 20, JobFunctionName: 'Telecom / Service Provider' },
+        { JobFunctionID: 21, JobFunctionName: 'Headhunter & Recruiter' }
+      ]).then(() => {
+        console.log("JobFunctions Added");
+        db['JobType'].bulkCreate([
+          { JobTypeID: 1, JobType: 'Full Time' },
+          { JobTypeID: 2, JobType: 'Part Time' },
+          { JobTypeID: 3, JobType: 'Internship' }
+        ]).then(() => {
+          console.log("JobTypes Added");
+          db['Currency'].bulkCreate([
+            { CurrencyID: '1', CurrencyCode: 'SGD', Symbol: '$' },
+            { CurrencyID: '2', CurrencyCode: 'JPY', Symbol: '¥' },
+            { CurrencyID: '3', CurrencyCode: 'USD', Symbol: '$' },
+            { CurrencyID: '4', CurrencyCode: 'KRW', Symbol: '₩' },
+            { CurrencyID: '5', CurrencyCode: 'EUR', Symbol: '€' },
+            { CurrencyID: '6', CurrencyCode: 'AUD', Symbol: '$' },
+            { CurrencyID: '7', CurrencyCode: 'MYR', Symbol: 'RM' },
+            { CurrencyID: '8', CurrencyCode: 'CNY', Symbol: '¥' }
+          ]).then(() => {
+            console.log("Currencies Added");
+            db['Country'].bulkCreate([
+              { CountryID: '1', CountryName: 'Singapore' },
+              { CountryID: '2', CountryName: 'Japan' },
+              { CountryID: '3', CountryName: 'Korea' },
+              { CountryID: '4', CountryName: 'Malaysia' },
+              { CountryID: '5', CountryName: 'Indonesia' },
+              { CountryID: '6', CountryName: 'Australia' }
+            ]).then(() => {
+              console.log("Countries Added");
+            })
+          })
+        })
+      })
+    })
+  }
 })
-// .then(() => {
-//   return [
-//     db['Job'].bulkInsert([
-//       { JobTitle: 'Job1', JobDescription: 'This Job 1' },
-//       { JobTitle: 'Job2', JobDescription: 'This Job 2' }
-//     ]),
-//     db['Industry'].bulkCreate([
-//       { IndustryName: 'Aerospace / Aviation' },
-//       { IndustryName: 'Agriculture / Forestry / Fishing' }
-//     ])
-//   ];
-// })
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
