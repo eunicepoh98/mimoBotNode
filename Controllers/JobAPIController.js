2217754864f38be118832e2dabf5a801c4ea0786
inbfi1niate4 <eunicepoh98@yahoo.com.sg>
2017-07-21 19:55:43 +0800

    api to add job with company info

--- Controllers/JobAPIController.js
+++ Controllers/JobAPIController.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 var job = require(path.resolve('./APIs/job.js'));
+var company = require(path.resolve('./APIs/company.js'));
 
 /** [GET] 
  * Get all Jobs
@@ -40,7 +41,7 @@ router.get('/', function (req, res, next) {
 /** 
  * [POST]
  * Add Job
- * http://localhost:3000/api/job
+ * http://localhost:3000/api/job/add
  * Body: JSON(application/json)
  * {
 	    "JobTitle": "",
@@ -64,7 +65,7 @@ router.post('/add', function (req, res, next) {
     SalaryFrom: req.body.SalaryFrom,
     SalaryTo: req.body.SalaryTo,
     CurrencyID: req.body.CurrencyID
-  }
+  };
   var newJob = {
     JobTitle: req.body.JobTitle,
     JobDescription: req.body.JobDescription,
@@ -86,12 +87,72 @@ router.post('/add', function (req, res, next) {
     });
 });
 
+/** 
+ * [POST]
+ * Add Job
+ * http://localhost:3000/api/job
+ * Body: JSON(application/json)
+ * {
+	    "JobTitle": "",
+      "JobDescription": "",
+      "JobQualification": "",
+      "JobResponsibilities": "",
+      "JobPostalCode": "",
+      "JobAddress": "",
+      "SalaryFrom": 600,
+      "SalaryTo": 800,
+      "CurrencyID": 1,
+      "JobTypeID": 1,
+      "IndustryID": [],
+      "JobFunctionID": [],
+      "CompanyName": "",
+      "CompanyAddress": "",
+      "CompanyPostalCode": ""
+   }
+*/
+router.post('/', function (req, res, next) {
+  var salary = {
+    SalaryFrom: req.body.SalaryFrom,
+    SalaryTo: req.body.SalaryTo,
+    CurrencyID: req.body.CurrencyID
+  }
+  var newJob = {
+    JobTitle: req.body.JobTitle,
+    JobDescription: req.body.JobDescription,
+    JobQualification: req.body.JobQualification,
+    JobResponsibilities: req.body.JobResponsibilities,
+    JobPostalCode: req.body.JobPostalCode,
+    JobAddress: req.body.JobAddress,
+    JobTypeID: req.body.JobTypeID
+  };
+  var com = {
+    CompanyName: req.body.CompanyName,
+    CompanyAddress: req.body.CompanyAddress,
+    CompanyPostalCode: req.body.CompanyPostalCode
+  };
+  company.addCompany(com)
+    .then(function (comdata) {
+      newJob.CompanyID = comdata.CompanyID;
+      job.addJob(salary, newJob, req.body.IndustryID, req.body.JobFunctionID)
+        .then(function (data) {
+          var response = { success: true, result: JSON.parse(data) };
+          res.status(200).send(response);
+        }).catch(function (error) {
+          var response = { success: false, message: error };
+          res.send(response);
+        });
+    }).catch(function (error) {
+      var response = { success: false, message: error };
+      res.send(response);
+    });
+});
+
 /* Get One Job by Id
  * http://localhost:3000/api/job/1
  * Params: /id
  * id - id of the job
 */
-router.get('/one/:id', function (req, res, next) {
+router.get('/:id', function (req, res, next) {
   job.getOneJob(req.params.id)
     .then(function (data) {
       var response = { success: true, result: JSON.parse(data) };
@@ -102,18 +163,5 @@ router.get('/one/:id', function (req, res, next) {
     });
 });
 
-/* Get Filtered Jobs
- * http://localhost:3000/api/job/filter
-*/
-// router.post('/filter', function (req, res, next) {
-//   job.getUserJob(['Aerospace', 'Construction'], [], [])
-//     .then(function (data) {
-//       var response = { success: true, result: JSON.parse(data) };
-//       res.status(200).send(response);
-//     }).catch(function (error) {
-//       var response = { success: false, message: error };
-//       res.send(response);
-//     });
-// });
 
 module.exports = router;
